2aebd2ff6a14be78fa27ba3c5c0136bb397df0c4
Kyle Walker <KyleJamesWalker@gmail.com>
2022-06-14 16:05:31 -0700

    Update for latest

--- process.sh
+++ process.sh
@@ -4,6 +4,7 @@
 if ! xcode-select -p > /dev/null ; then
 	echo Installing XCode Command Line Tools
 	xcode-select --install
+	read -p "Press enter to continue when Xcode has finished installing"
 else
 	echo XCode Command Line Tools Already installed
 fi
@@ -11,7 +12,9 @@ fi
 # Install brew
 if ! which brew > /dev/null ; then
 	echo Installing HomeBrew
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kyle.walker/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 else
 	echo Homebrew Already installed
 fi
@@ -19,13 +22,16 @@ fi
 # Install some base tools
 brew doctor
 brew update
-brew install wget vim python thefuck git jq tmux asciinema tree jfrog-cli-go terragrunt terraform npm aws-okta
-pip3 install --upgrade pip setuptools wheel
+brew install wget vim python thefuck git jq tmux asciinema tree jfrog-cli npm aws-okta docker docker-compose colima zsh
+pip3 install --upgrade pip setup-tools wheel
+
+# Set shell to zsh
+chsh -s /usr/local/bin/zsh
 
 # Install Oh My ZSH!
 if [ ! -d ~/.oh-my-zsh ]; then
 	echo Installing Oh My ZSH!
-	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	cp .zshrc ~
 	cp .zshrc-creds ~
 else
@@ -35,10 +41,10 @@ fi
 if ! which pyenv > /dev/null ; then
 	echo Installing pyenv
 	brew install pyenv pyenv-virtualenv
-	pyenv install 2.7.9
-	pyenv install 3.6.6
-	pyenv install 3.7.0
-	pyenv virtualenv 3.6.6 genv
+	pyenv install 3.8.13
+	pyenv install 3.9.13
+	pyenv install 3.10.4
+	pyenv virtualenv 3.10.4 genv
 	pyenv global genv
 else
 	echo pyenv Already Installed
@@ -63,48 +69,17 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 
 brew tap homebrew/cask-versions
 
-# Removed: boostnote path-finder keybase caskroom/versions/java8
-brew install --cask iterm2 visual-studio-code beyond-compare caffeine docker droplr homebrew/cask-fonts/font-hack dbeaver-community
+# Install some standard tools
+brew install --cask iterm2 visual-studio-code beyond-compare caffeine droplr homebrew/cask-fonts/font-hack dbeaver-community
 
 brew install swagger-codegen gradle
 
-# Install vscode extensions (get the list from the following commands)
-# code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension alexcvzz.vscode-sqlite
-code --install-extension bajdzis.vscode-database
-code --install-extension donjayamanne.githistory
-code --install-extension donjayamanne.jupyter
-code --install-extension dracula-theme.theme-dracula
-code --install-extension eamodio.gitlens
-code --install-extension formulahendry.code-runner
-code --install-extension GrapeCity.gc-excelviewer
-code --install-extension hashicorp.terraform
-code --install-extension HookyQR.beautify
-code --install-extension jakeboone02.cypher-query-language
-code --install-extension jithurjacob.nbpreviewer
-code --install-extension lextudio.restructuredtext
-code --install-extension magicstack.MagicPython
-code --install-extension mikestead.dotenv
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension searKing.preview-vscode
-code --install-extension Shan.code-settings-sync
-code --install-extension shd101wyy.markdown-preview-enhanced
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension vscodevim.vim
-code --install-extension waderyan.gitblame
-
 if ! which kr > /dev/null ; then
-	# Install and pair Krypton
-	curl https://krypt.co/kr | sh
+	# Install and pair Krypton (hack to install on latest version of OSX)
+	curl https://krypt.co/kr | sed "s/11.0/12.2/g" | sh
 	kr pair
 	kr github
 	kr codesign
 else
-	echo pyenv Already Installed
+	echo kr Already Installed
 fi
