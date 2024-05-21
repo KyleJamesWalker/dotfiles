9ddc7833562407b0d77d6e4cfc880ab71cd287b7
Kyle James Walker (he/him) <KyleJamesWalker@gmail.com>
2024-05-21 14:56:43 -0700

    Update process.sh

--- process.sh
+++ process.sh
@@ -11,10 +11,12 @@ fi
 
 # Install brew
 if ! which brew > /dev/null ; then
-	echo Installing HomeBrew
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo Installing HomeBrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$USER/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-	echo Homebrew Already installed
+  echo Homebrew Already installed
 fi
 
 # Install some base tools
@@ -28,32 +30,32 @@ chsh -s /usr/local/bin/zsh
 
 # Install Oh My ZSH!
 if [ ! -d ~/.oh-my-zsh ]; then
-	echo Installing Oh My ZSH!
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	cp .zshrc ~
-	cp .zshrc-creds ~
+  echo Installing Oh My ZSH!
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  cp .zshrc ~
+  cp .zshrc-creds ~
 else
-	echo Oh My ZSH! Already Installed
+  echo Oh My ZSH! Already Installed
 fi
 
 if ! which pyenv > /dev/null ; then
-	echo Installing pyenv
-	brew install pyenv pyenv-virtualenv
-	pyenv install 3.8.13
-	pyenv install 3.9.13
-	pyenv install 3.10.4
-	pyenv install 3.11.9
-	pyenv install 3.12.2
-	pyenv virtualenv 3.12.2 genv
-	pyenv global genv
+  echo Installing pyenv
+  brew install pyenv pyenv-virtualenv
+  pyenv install 3.8.13
+  pyenv install 3.9.13
+  pyenv install 3.10.4
+  pyenv install 3.11.9
+  pyenv install 3.12.2
+  pyenv virtualenv 3.12.2 genv
+  pyenv global genv
 else
-	echo pyenv Already Installed
+  echo pyenv Already Installed
 fi
 
 # Install vundle
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	cp .vimrc ~
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  cp .vimrc ~
 fi
 
 # Copy Configs
